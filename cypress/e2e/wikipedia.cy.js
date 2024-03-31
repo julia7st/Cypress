7f996fbcdf1450d2aec7eb3e2fa1562516ffc35b
julia7st <juliastepanova10@gmail.com>
2024-03-31 19:54:31 +0300

    Added tests for HW23

--- cypress/e2e/wikipedia.cy.js
+++ cypress/e2e/wikipedia.cy.js
@@ -1,5 +1,21 @@
+/// <reference types="cypress" />
+
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  beforeEach(() => {
+    cy.visit('https://www.wikipedia.org/')
+    cy.get('#js-link-box-en > strong').click();
+  })
+
+  it('Verify the Login button is visible', () => {
+    cy.get('#pt-login-2 > a > span').should('be.visible');
+  })
+
+  it('Verify the Create Account button is visible', () => {
+    cy.get('#pt-createaccount-2 > a > span').should('be.visible');
+
+  })
+
+  it('Verify the Vector user links dropdown is visible', () => {
+    cy.get('#vector-user-links-dropdown').should('be.visible');
   })
 })
